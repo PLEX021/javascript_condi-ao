884dd1b7408829a0ebd1eebcc53c03dabd77faba
PLEX021 <eduardojefersom123@gmail.com>
2024-12-02 20:24:47 -0300

    exercicio 18 corrigido

--- exer18.js
+++ exer18.js
@@ -1,5 +1,5 @@
 const readlineSync = require('readline-sync');
-const base1= Number(readlineSync.question ("Qual e a base detse retangulo?"));
+const base1= Number(readlineSync.question ("Qual e a base deste retangulo?"));
 const altura1 = Number(readlineSync.question ("Qual e a altura deste retagulo?"));
 const base2= Number(readlineSync.question ("Qual e a base detse retangulo?"));
 const altura2 = Number(readlineSync.question ("Qual e a altura deste retagulo?")) 
@@ -7,14 +7,14 @@ const altura2 = Number(readlineSync.question ("Qual e a altura deste retagulo?")
 const area1=base1*altura1;
 const area2=base2*altura2;
 
-if(area1 > area2){
+if(area1 == area2){
+    console.log("os dois retangulos tem as mesmas areas")
+}
+ else if(area1 > area2){
     console.log("O primeiro retangulo tem a area maior que a do segundo");
 }
- if(area2 > area1){
+ else if(area2 > area1){
     console.log("O segundo retangulo tem a rea maior que a do primero");
-}
-else if(area1 == area2){
-    console.log("Os dois tem as mesmas areas")
 }
 else{
     console.log("erro")
